676615365f7d5c3494870ef86e65540753b57d47
Nasrul Wahabi <wahabinasrul@gmail.com>
2021-12-14 22:01:58 +0700

    Delete Project_Akhir directory

--- Project_Akhir/Controller/SiswaApp.java
+++
@@ -1,5 +0,0 @@
-package com.javaoop.tugas.SMT_3.Project_Akhir.Controller;
-
-public class SiswaApp {
-    
-}
--- Project_Akhir/View/SiswaView.java
+++
@@ -1,5 +0,0 @@
-package com.javaoop.tugas.SMT_3.Project_Akhir.View;
-
-public class SiswaView {
-    
-}
